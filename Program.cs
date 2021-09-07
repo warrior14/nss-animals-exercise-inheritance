5fd0eec356db534fa8245503400297cdb2149fad
warrior14 <ironaddicts14@gmail.com>
2021-09-07 13:31:51 -0500

    refactored Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Inheritnace.Animals;
 
 namespace AnimalsExercise
 {
@@ -6,7 +7,20 @@ namespace AnimalsExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tiger luke = new Tiger { Name = "Luke", Furlength = 14 };
+            Cat jerry = new Cat { Name = "Jerry", Furlength = 21};
+            Dog buddy = new Dog { Name = "Buddy", Furlength = 13};
+            Snake viper = new Snake { Name = "Viper"};
+
+
+        Console.Write($"{luke.Name} speaks");
+            luke.Roar();
+        Console.Write($"{jerry.Name} speaks");
+            jerry.Meow();
+        Console.Write($"buddy.Name" speaks);
+            buddy.Bark();
+        Console.Write($"{viper.Name}");
+            viper.Hiss();
         }
     }
 }
